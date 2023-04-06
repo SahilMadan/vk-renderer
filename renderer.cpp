6727501f2320c6cd94e1ed8eb8d8d42533acc332
Sahil Madan <Hillatio@gmail.com>
2023-04-06 11:28:14 -0700

    Fix projection

--- renderer.cpp
+++ renderer.cpp
@@ -642,6 +642,7 @@ void Renderer::Draw() {
     glm::mat4 view = glm::translate(glm::mat4(1.f), camera_position);
     glm::mat4 projection =
         glm::perspective(glm::radians(70.f), 1700.f / 900.f, 0.1f, 200.f);
+    projection[1][1] *= -1;
     glm::mat4 model = glm::rotate(
         glm::mat4(1.f), glm::radians(framenumber_ * 0.4f), glm::vec3(0, 1, 0));
     glm::mat4 mesh_matrix = projection * view * model;
